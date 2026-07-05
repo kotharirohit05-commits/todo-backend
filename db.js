61ca58dd3a8549415d8637bc0fe57dc8ed284733
kotharirohit05-commits <kothari.rohit05@gmail.com>
2026-07-05 12:49:11 +0530

    Configure backend for Neon database

--- db.js
+++ db.js
@@ -1,34 +1,22 @@
 const { Client } = require("pg");
 
-
 const client = new Client({
-
-    host: "localhost",
-
-    user: process.env.DB_USER,
-
-    database: process.env.DB_NAME,
-
-    password: process.env.DB_PASSWORD,
-
-    port: 5432
-
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
-
 
 client.connect()
-
-.then(() => {
-
+  .then(() => {
     console.log("PostgreSQL Connected");
-
-})
-
-.catch((error) => {
-
+  })
+  .catch((error) => {
     console.log(error);
-
-});
-
+  });
 
 module.exports = client;
